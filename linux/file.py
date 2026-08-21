56d0492f219018d2e253c54dd1f97e898650b722
psusannaabhishiktha <psusannaabhishiktha@gmail.com>
2026-08-21 18:30:29 +0530

    success

--- linux/file.py
+++ linux/file.py
@@ -46,5 +46,6 @@ def merge(left, right):
     return result
 if __name__ == "__main__":
     # Example usage
+    arr = [38, 27, 43, 3, 9, 82, 10]
     sorted_arr = merge_sort(arr)
     print("Sorted array:", sorted_arr)
